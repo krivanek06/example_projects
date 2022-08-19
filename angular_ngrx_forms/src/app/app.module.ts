2deabcaae82fb2b8fab5e5e9feffdbae90d7efbd
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-08-19 20:28:36 +0200

    feat(ngrx-forms): installed libs

--- angular_ngrx_forms/src/app/app.module.ts
+++ angular_ngrx_forms/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule,
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]

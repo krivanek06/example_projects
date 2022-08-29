6eb4236565f6dc602d076c26313f23caf9e83498
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-08-29 19:53:31 +0200

    fix: fixing angular elements for standalone components

--- angular-elements/src/main.ts
+++ angular-elements/src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+import { createApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { VotingNewComponent } from './app/voting-new/voting-new.component';
 
 import { environment } from './environments/environment';
 
@@ -9,11 +12,22 @@ if (environment.production) {
 }
 
 // old way of bootstraping
-// platformBrowserDynamic()
-// 	.bootstrapModule(AppModule)
-// 	.catch((err) => console.error(err));
+platformBrowserDynamic()
+	.bootstrapModule(AppModule)
+	.catch((err) => console.error(err));
 
-// new way of bootstraping
-bootstrapApplication(AppComponent, {
-	providers: [],
-}).catch((err) => console.error(err));
+// --------------------------------------------------------
+// PR: https://github.com/angular/angular/pull/46475
+// get a hand on the `ApplicationRef` to access its injector
+createApplication({ providers: [] }).then((appRef) => {
+	// create a constructor of a custom element
+	const votingNew = createCustomElement(VotingNewComponent, { injector: appRef.injector });
+
+	// register in a browser
+	customElements.define('voting-new', votingNew);
+});
+
+// new way of bootstraping <== this does not work in when creating Angular elements
+// bootstrapApplication(AppComponent, {
+// 	providers: [],
+// }).catch((err) => console.error(err));

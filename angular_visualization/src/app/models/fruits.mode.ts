b0a9c40164bb80cc588f44bc8137f1fcb903eafe
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-02 08:23:52 +0300

    feat: added model data for basic charts

---
+++ angular_visualization/src/app/models/fruits.mode.ts
@@ -0,0 +1,15 @@
+// pie chart
+export const FRUIT_TYPES = [
+	{ name: 'Apple', total: 5 },
+	{ name: 'Orange', total: 12 },
+	{ name: 'Kiwi', total: 9 },
+	{ name: 'Mango', total: 2 },
+	{ name: 'Banana', total: 1 },
+	{ name: 'Avokado', total: 10 },
+];
+
+// DONUT chart
+export const FRUIT_TYPES_TOTAL = {
+	stock: FRUIT_TYPES.length,
+	capacity: FRUIT_TYPES.length + 12,
+};
---
+++ angular_visualization/src/app/models/index.ts
@@ -0,0 +1,11 @@
+/*
+  Buble chart             - Earnings
+  Line chart + Bar chart  - Inflation
+  Financial chart         - Financial
+  Donut chart + Pie chart - Fruits
+*/
+
+export * from './earnings.model';
+export * from './financial.model';
+export * from './fruits.mode';
+export * from './inflation.model';

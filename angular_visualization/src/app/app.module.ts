37dabf264cb64e9d35f689a6f4362071540f2a20
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-04 09:40:42 +0300

    feat(vizualization): installed libraries

--- angular_visualization/src/app/app.module.ts
+++ angular_visualization/src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GoogleChartsModule } from 'angular-google-charts';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { NgChartsModule } from 'ng2-charts';
+import { NgxEchartsModule } from 'ngx-echarts';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		// https://www.npmjs.com/package/ng2-charts
+		NgChartsModule,
+		// https://www.npmjs.com/package/ngx-echarts
+		NgxEchartsModule.forRoot({
+			/**
+			 * This will import all modules from echarts.
+			 * If you only need custom modules,
+			 * please refer to [Custom Build] section.
+			 */
+			echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+		}),
+		// https://www.npmjs.com/package/highcharts-angular
+		HighchartsChartModule,
+		// https://www.npmjs.com/package/angular-google-charts
+		GoogleChartsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}

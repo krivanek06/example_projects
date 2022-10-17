4002b30887d36bda76a584773537b0f7a0f3d9c0
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-17 07:33:52 +0300

    feat(angular-memo): added config

---
+++ angular_memo/src/app/material.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+	imports: [
+		MatListModule,
+		MatIconModule,
+		MatButtonModule,
+		MatRippleModule,
+		MatDividerModule,
+		MatFormFieldModule,
+		MatDialogModule,
+		MatTooltipModule,
+		MatInputModule,
+		MatCardModule,
+		MatBadgeModule,
+		MatProgressSpinnerModule,
+		MatRadioModule,
+	],
+	exports: [
+		MatListModule,
+		MatIconModule,
+		MatButtonModule,
+		MatRippleModule,
+		MatDividerModule,
+		MatFormFieldModule,
+		MatDialogModule,
+		MatTooltipModule,
+		MatInputModule,
+		MatCardModule,
+		MatBadgeModule,
+		MatProgressSpinnerModule,
+		MatRadioModule,
+	],
+})
+export class MaterialModule {}

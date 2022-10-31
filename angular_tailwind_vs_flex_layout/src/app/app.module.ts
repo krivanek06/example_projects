90f4d681ec6f7ed9f5f83f39ab7939eda810bcb2
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-10-31 22:13:33 +0200

    feat(tailwind-vs-flex-layout): added libs

--- angular_tailwind_vs_flex_layout/src/app/app.module.ts
+++ angular_tailwind_vs_flex_layout/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexModule } from '@angular/flex-layout/flex';
+import { GridModule } from '@angular/flex-layout/grid';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FlexModule, GridModule, FlexLayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

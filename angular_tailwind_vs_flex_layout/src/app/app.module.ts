7f77f89502776ac02559354c33b41692a38bc33d
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-11-01 20:25:52 +0200

    feat: added examples fxflex & tailwind

--- angular_tailwind_vs_flex_layout/src/app/app.module.ts
+++ angular_tailwind_vs_flex_layout/src/app/app.module.ts
@@ -4,10 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FlexModule } from '@angular/flex-layout/flex';
 import { GridModule } from '@angular/flex-layout/grid';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ExampleFlexLayoutComponent } from './example-flex-layout/example-flex-layout.component';
+import { ExampleTailwindComponent } from './example-tailwind/example-tailwind.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FlexModule, GridModule, FlexLayoutModule],
+  declarations: [
+    AppComponent,
+    ExampleFlexLayoutComponent,
+    ExampleTailwindComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FlexModule,
+    GridModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

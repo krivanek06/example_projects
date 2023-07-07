e443dc44441bb7713e99b14113d2ec451c9eff38
Eduard Krivanek <krivanek1234@gmail.com>
2023-07-07 15:53:20 +0300

    chore: removed top level - lighweight injection token folder

--- angular_lightweight_injection_token/src/app/example-inject/models/token.model.ts
+++
@@ -1,7 +0,0 @@
-export const LOGGER_PROVIDER_TOKEN = 'LOGGER_PROVIDER_TOKEN';
-
-export interface LOGGER {
-  logNormal: () => void;
-
-  logWarning: () => void;
-}
--- angular_lightweight_injection_token/src/app/example_widget/model/width.model.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export interface WidgetInterface {
-  refresh: () => void;
-}
-
-
-export const WIDGET = new InjectionToken<WidgetInterface>('WidgetInterface');

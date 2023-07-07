832384093f964904768ebaee95947366028131c8
Eduard Krivanek <krivanek1234@gmail.com>
2023-07-07 15:34:09 +0300

    chore: removed top level - angular_forms

--- angular_forms/src/app/advanced-forms/advanced-forms.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-advanced-forms',
-	templateUrl: './advanced-forms.component.html',
-	styleUrls: ['./advanced-forms.component.scss'],
-})
-export class AdvancedFormsComponent {}
--- angular_forms/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	content: ['./src/**/*.{html,ts}'],
-	important: true, // to overwride angular material
-	darkMode: 'class', // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	plugins: [require('@tailwindcss/typography')],
-};

56d11ec28dab1cb8292dbaffc4337c3aa73979c0
Eduard Krivanek <krivanek1234@gmail.com>
2023-07-07 22:29:46 +0300

    chore: removed some libs top level

--- angular_nx_example/libs/ui-components/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/button-wrapper.directive';
-export * from './lib/buttons';
-export * from './lib/ui-components.module';
-export * from './lib/ui-useless-component/ui-useless-component.module';
--- angular_nx_example/libs/unused-lib-1/README.md
+++
@@ -1,7 +0,0 @@
-# unused-lib
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test unused-lib` to execute the unit tests.
